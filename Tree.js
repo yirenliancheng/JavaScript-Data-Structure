0a3fdf262b1aaf36ae1d561c7cff54ee7aed2077
yirenliancheng <761101850@qq.com>
2020-04-14 14:56:45 +0800

    增加了遍历树的迭代方法

--- Tree.js
+++ Tree.js
@@ -89,7 +89,7 @@ class Tree{
    }
 //中序遍历递归实现
    inOrderTraverse(){
-         this.inorder(this.root);
+       this.inorder(this.root);
    }
 
    inorder(node){
@@ -99,6 +99,28 @@ class Tree{
            this.inorder(node.right);
        };
    }
+
+//中序遍历迭代实现
+    inOrderDiedai(){
+        let node=this.root;
+        let res=[];
+        let stack=[];
+        while(node){
+            stack.push(node);
+            node = node.left;
+        }
+        while(stack.length!==0||node){
+            if(node){
+                stack.push(node);
+                node=node.left;
+            }else{
+                node=stack.pop();
+                res.push(node.val);
+                node=node.right;
+            }
+        }
+        return res;
+    }
 
 //前序遍历递归实现
     preOrderTraverse(){
@@ -111,34 +133,87 @@ class Tree{
            this.preOrder(node.right);
        }
     }
+//前序遍历迭代实现
+//先压右边的，再压入左边的
+    preOrderDiedai(){
+        let res=[];
+        let stack=[];
+        let node=this.root;
+        if(node==null){
+            return res;
+        }
+        stack.push(node);
+        while(stack.length!==0){    
+            node=stack.pop();
+            res.push(node.val);
+            if(node.right){
+                stack.push(node.right);
+            }
+            if(node.left){
+                stack.push(node.left);
+            }
+        }
+        return res;
+    }
 
 //后序遍历递归实现
 postOrderTraverse(){
-    this.postOrder(this.root);
+    this.postOrder(this.root); 
 }
 postOrder(node){
-   if(node!==null){
-       this.preOrder(node.left);
-       this.preOrder(node.right);
+ if(node){
+       this.postOrder(node.left);
+       this.postOrder(node.right);
        console.log(node.val);
    }
 }
+
+//后序遍历迭代实现,需要两个指针，第二个指针代表当前的节点是否已经被访问了
+ postOrderDiedai(){
+     let res=[];
+     let stack=[];
+     let node=this.root;
+     let lastVisit=null;
+     if(node==null) return res;
+     while(node){
+         stack.push(node);
+         node=node.left;
+     }
+
+     while(stack.length!==0){
+         node=stack.pop();
+         if(node.right===null||node.right===lastVisit){
+             res.push(node.val);
+             lastVisit=node;
+         }else{
+             stack.push(node);
+             node=node.right;
+             while(node){
+                 stack.push(node);
+                 node=node.left;
+             }
+         }
+     
+     }
+    return res;
+ }
 }
 
 var tree= new Tree();
 tree.insert(11);
 tree.insert(7);
-tree.insert(3);
-tree.insert(9);
 tree.insert(5);
+tree.insert(9);
+tree.insert(3);
+tree.insert(6);
 tree.insert(8);
 tree.insert(10);
-tree.insert(12);
-tree.insert(13);
-tree.insert(14);
 tree.insert(15);
-tree.insert(18);
+tree.insert(13);
+tree.insert(12);
+tree.insert(14);
 tree.insert(20);
+tree.insert(18);
 tree.insert(25);
 
 console.log(tree.find(5));
@@ -149,3 +224,6 @@ console.log(tree);
 tree.inOrderTraverse();
 tree.preOrderTraverse();
 tree.postOrderTraverse();
+console.log(tree.inOrderDiedai());
+console.log(tree.preOrderDiedai());
+console.log(tree.postOrderDiedai());
